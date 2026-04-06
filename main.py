1286773119459c7e0c9b37ccda9200b35ad18859
Matthieu <ulisym320@gmail.com>
2026-04-06 18:27:59 +0200

    hjbh

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from moteur import Game
 import pyglet
 
-game = Game()
-pyglet.app.run()
-
 pygame.init()
 pygame.mixer.init()
 
 pygame.mixer.music.load("PIANO.wav")
 pygame.mixer.music.play(-1)
+
+game = Game()
+pyglet.app.run()
+

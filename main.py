06ef3c4de9bee9baa667d2fbcd485207de33a2fc
Matthieu <ulisym320@gmail.com>
2026-04-06 19:51:52 +0200

    hjbh

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ pygame.mixer.music.play(-1)
 pygame.mixer.music.set_volume(0.5)
 son_pas = pygame.mixer.Sound("musique/bruit_de_pas.wav")
 son_saut = pygame.mixer.Sound("musique/saut.flac")
+#il faut mettre ces sons quand le boug saute ou marche
 
 game = Game()
 pyglet.app.run()

260aaa39c73b576746627446647babbabf09eafb
Matthieu <ulisym320@gmail.com>
2026-04-07 12:09:02 +0200

    c'est sur pygletmaintenant

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from moteur import Game
 import pyglet
-import pygame
 
+musique = pyglet.media.load("musique/PIANO.wav")
+player_musique = pyglet.media.Player()
+player_musique.queue(musique)
+player_musique.loop = True
+player_musique.volume = 0.5
+player_musique.play()
 
-pygame.mixer.init()
-pygame.mixer.music.load("musique/PIANO.wav")
-pygame.mixer.music.play(-1)
-pygame.mixer.music.set_volume(0.5)
-son_pas = pygame.mixer.Sound("musique/bruit_de_pas.wav")
-son_saut = pygame.mixer.Sound("musique/saut.flac")
+son_pas = pyglet.media.load("musique/bruit_de_pas.wav", streaming=False)
+son_saut = pyglet.media.load("musique/saut.flac", streaming=False)
 #il faut mettre ces sons quand le boug saute ou marche
 
 game = Game()
